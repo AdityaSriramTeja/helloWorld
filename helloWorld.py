15a82b6a00888d313af48543b7c0ae367cbd9a52
AdityaSriramTeja <adityasriramteja@gmail.com>
2021-07-24 23:59:41 -0400

    added comments and if condition

--- helloWorld.py
+++ helloWorld.py
@@ -1,15 +1,19 @@
+#print statements
 print("Hello world")
 print("How are you doing today")
+#arithmetic satements 
 x = 1
 y= 2
 print(x+y)
 print (y-x)
 print(y*x)
 print(y/x)
+#input
 name = input("what is your name? ")
 print(name)
 age = input("what is your age? ")
 print(name + " is " + age + " years old")
-if(age > 10):
+#if condition
+if(age < 10):
     print(name + " is in elementary school")
 
